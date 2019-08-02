dac1475589751441db545dded8621608ccec91c1
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-02 11:44:00 -0700

    pass update test

--- lib/project.rb
+++ lib/project.rb
@@ -41,6 +41,29 @@ class Project
     self.id = new_id
   end
 
+  def update(attr_hash)
+    # ok so...
+    # this is an overkill solution, but kind of cool because it allows for partial upating of attributes, AKA an incomplete attr hash, like if a project had an attribute such as @city
+    #go through the input hash and for each key
+    attr_hash.each_key do |key|
+      # if the methods on self include the key, so include? :title => true
+      if self.methods.include? key.to_sym
+        # then, execute that method on the object!
+        # using the value from the hash at that key
+        # attr_hash[:title] => "Project Title"
+        # self.title= "Project Title"
+        self.public_send(key.to_s + "=", attr_hash[key])
+        #then update the database
+        binding.pry
+        # don't want to try and change the ID of the object because the ID should be a permanent value, and ID is technically an accessor right now
+        if key != :id
+          DB.exec("UPDATE projects SET #{key.to_s} = '#{attr_hash[key]}' WHERE id=#{self.id.to_i} ;")
+        end
+      end
+    end
+
+  end
+
   def volunteers
     result = DB.exec("SELECT * FROM volunteers WHERE project_id='#{self.id}'")
     volunteers = []
